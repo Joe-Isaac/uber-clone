2088d589b92e67370f59ba35813733646bfbe92b
joe-isaac <isaacjoseph950@gmail.com>
2022-11-21 05:26:46 +0300

    Uber clone

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
+//Set up REDUX
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+
+    <HomeScreen/>
+
+    </Provider>
   );
 }
 
---
+++ components/NavOptions.js
@@ -0,0 +1,47 @@
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+
+const data = [
+    {
+        id: '123',
+        title: "Get a ride",
+        image: 'https://links.papareact.com/3pn',
+        screen: "MapScreen"
+    },
+    {
+        id: '456',
+        title: "Order food",
+        image: 'https://links.papareact.com/28w',
+        screen: "EatScreen"
+    },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+    keyExtractor={(item) => item.id}
+    data={data}
+    horizontal
+    renderItem={({item}) => (
+        <TouchableOpacity
+        style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+            <View>
+                <Image
+                style = {{ width: 120, height: 120, resizeMode:"contain"}}
+                source = {{ uri : item.image}}
+                />
+                <Text
+                style={tw`mt-2 text-lg font-semibold`}
+                >
+                    {item.title}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    )}
+    />
+  )
+}
+
+export default NavOptions
